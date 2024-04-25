db0aeb2deb85fa3873b3147e21d55d1e8937f948
Sharath-kumar-H-R <sharathkumarhr3@gmail.com>
2024-04-25 11:49:00 +0530

    nav bar created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+         "color-green":"#1A4D2E",
+         "color-primary":"#01051e",
+         "color-primary-light":"#02076",
+         "color-primary-dark":"#010417",
+         "color-gray":"#333",
+         "color-white":"#fff",
+         "color-blob":"#A427DF",
+      }
+    },
+    container:{
+      center:true,
+      padding: {
+        default: '20px',
+        md: '50px',
+      }
+    }
   },
   plugins: [],
 }
